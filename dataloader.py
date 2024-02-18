397bda67c01c33328ca7546a349e69dbc9f67bba
HwangIsAce <esteen196@naver.com>
2024-02-18 11:26:52 +0900

    feat masking a certain precentage of tokens

--- dataloader.py
+++ dataloader.py
@@ -71,17 +71,48 @@ def collate_fn(batch):
     # max_len = max([len(sample['input_ids']) for sample in batch])
 
     for sample in batch:
+
+        # padding
         diff = max_len - len(sample['input_ids'])
-        if diff > 0 :
-            zero_pad = torch.zeros(size= (diff,))
+        
+        zero_pad = torch.zeros(size= (diff,))
 
-            collate_input_ids.append(torch.cat([sample['input_ids'].view([len(sample['input_ids'])]), zero_pad], dim=0))
-            collate_labels.append(torch.cat([sample['labels'].view([len(sample['labels'])]), zero_pad], dim=0))
-        else :
-            collate_input_ids.append(sample['input_ids'].view(len(sample['input_ids'])))
-            collate_labels.append(sample['labels'].view(len(sample['labels'])))
+        collate_input_ids.append(torch.cat([sample['input_ids'].view([len(sample['input_ids'])]), zero_pad], dim=0))
+        collate_labels.append(torch.cat([sample['labels'].view([len(sample['labels'])]), zero_pad], dim=0))
 
-    return {'input_ids': torch.stack(collate_input_ids), 'labels' : torch.stack(collate_labels)}
+        input_ids = torch.stack(collate_input_ids)
+        labels = torch.stack(collate_labels)
+
+        # masking
+        masking_prob =0.15
+        full_mask = torch.randn(input_ids.shape) < masking_prob
+
+        special_tokens = [0, 1, 2, 3, 4]
+        for tk in special_tokens:
+            full_mask = full_mask & (input_ids != tk)
+
+        random_prob = 0.1
+        random_mask = torch.randn(input_ids.shape) < random_prob
+        full_mask_with_random = full_mask & (random_mask)
+
+        unchanged_prob = 0.1
+        unchanged_mask = torch.randn(input_ids.shape) < unchanged_prob
+        full_mask_with_unchanged = full_mask & (unchanged_mask)
+
+        full_mask_with_mask = full_mask & (~full_mask_with_random) & (~full_mask_with_unchanged)
+
+        final_mask = input_ids.clone()
+        num_random_tokens = full_mask_with_random.sum().item()
+        random_tokens = torch.randint(0, rb_config.bert_config['vocab_max_size'], size=(num_random_tokens,))
+        indices = torch.nonzero(full_mask_with_random, as_tuple=True)
+        final_mask[indices] = random_tokens.float()
+
+        mask_token = 4
+        final_mask = final_mask.masked_fill_(full_mask_with_mask, mask_token)
+
+        input_ids = final_mask
+    
+    return {'input_ids': input_ids, 'labels' : labels}
 
 def MyDataLoader(batch_size=rb_config.bert_config['batch_size'], data_path= rb_config.processed_data_folder + '/v3_ing_title_tag_sample/train.txt'):
 
